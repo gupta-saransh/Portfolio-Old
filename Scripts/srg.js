6cae938884c4da11613b16a0adc60fb14e4672a4
Saransh Gupta <35869697+srg3006@users.noreply.github.com>
2018-05-12 00:54:22 +0530

    Update srg.js

--- Scripts/srg.js
+++ Scripts/srg.js
@@ -5,3 +5,13 @@ function scrollWin() {
         function scrollWind() {
             window.scrollTo(0, 1140);
         }
+  var myVar;
+
+      function myFunction() {
+          myVar = setTimeout(showPage, 2500);
+      }
+
+      function showPage() {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("myDiv").style.display = "block";
+      }
