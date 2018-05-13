bfd3c71a6f50ad621b4b684536886e9a3ac2a049
Saransh Gupta <35869697+srg3006@users.noreply.github.com>
2018-05-13 13:16:07 +0530

    Update srg.js

--- Scripts/srg.js
+++ Scripts/srg.js
@@ -1,10 +1,4 @@
-function scrollWin() {
-            window.scrollTo(0, 596);
-        }
 
-        function scrollWind() {
-            window.scrollTo(0, 1140);
-        }
   var myVar;
 
       function myFunction() {
